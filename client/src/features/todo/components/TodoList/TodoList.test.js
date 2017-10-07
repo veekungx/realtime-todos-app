f97952c78d4e4b2b247b632d2b01c4a33f9bf4af
Ti <veekungx@gmail.com>
2017-10-07 16:45:29 +0700

    add TodoList test

---
+++ client/src/features/todo/components/TodoList/TodoList.test.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import TodoList from './TodoList';
+import TodoItem from '../TodoItem/TodoItem'
+import generateMockTodo from '../../../../helpers/generateMockTodo';
+
+describe('TodoList', () => {
+  it('should render');
+
+  describe('Props', () => {
+    it('should render todos', () => {
+      const todos = {
+        edges: [
+          generateMockTodo(),
+          generateMockTodo()
+        ]
+      };
+      const wrapper = shallow(<TodoList todos={todos} />);
+      expect(wrapper.find('TodoItem').length).toEqual(2);
+    });
+    it('should pass todo to TodoItem', () => {
+      const mockTodo = generateMockTodo();
+      const todos = {
+        edges: [mockTodo]
+      };
+      const wrapper = shallow(<TodoList todos={todos} />);
+      expect(wrapper.find('TodoItem').at(0).props().todo).toEqual(mockTodo);
+    });
+  });
+  describe('Events', () => {
+    it('should handle onDeleteTodo', () => {
+      const handler = jest.fn();
+      const mockTodo = generateMockTodo();
+      const todos = {
+        edges: [mockTodo]
+      };
+      const wrapper = shallow(<TodoList onDeleteTodo={handler} />)
+      wrapper.find('TodoItem').at(0).simulate('click');
+      expect(handler).toHaveBeenCalled();
+    });
+    it('should handle onToggleTodo', () => {
+      const handler = jest.fn();
+      const wrapper = shallow(<TodoList onToggleTodo={handler} />)
+      const mockTodo = generateMockTodo();
+      const todos = {
+        edges: [mockTodo]
+      };
+      expect(handler).toHaveBeenCalled();
+    });
+  })
+});
