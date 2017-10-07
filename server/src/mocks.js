74ea1dfcdb12f8c21e385cd578ad1cd29a6a08d5
Ti <veekungx@gmail.com>
2017-10-08 03:32:55 +0700

    add mock state to Todo type

--- server/src/mocks.js
+++ server/src/mocks.js
@@ -5,6 +5,7 @@ const casual = require('casual');
 module.exports = {
   Todo: () => ({
     title: casual.sentence,
+    state: casual.random_element(['TODO_ACTIVE', 'TODO_COMPLETED']),
   }),
   TodoConnection: () => ({
     edges: () => new MockList([5, 20]),
