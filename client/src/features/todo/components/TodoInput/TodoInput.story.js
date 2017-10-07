97424f2e5cf0ec62ff5f5154a334cb29b39cc4fd
Ti <veekungx@gmail.com>
2017-10-08 01:21:24 +0700

    add TodoInput story

---
+++ client/src/features/todo/components/TodoInput/TodoInput.story.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import TodoInput from './TodoInput';
+
+storiesOf('TodoInput', module)
+  .add('default', () => <TodoInput />)
+  .add('has text', () => <TodoInput value="Some Text" />)
+  .add('all todos completed', () => <TodoInput isComplatedAll />);
