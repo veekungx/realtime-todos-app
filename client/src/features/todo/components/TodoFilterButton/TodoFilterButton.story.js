739c326ed1608728bf4f8d32bdfe0b1477afacd4
Ti <veekungx@gmail.com>
2017-10-07 20:34:44 +0700

    TodoFilterButton story

---
+++ client/src/features/todo/components/TodoFilterButton/TodoFilterButton.story.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import TodoFilterButton from './TodoFilterButton';
+
+const buildStory = (attrs) => {
+  const props = {
+    label: 'Hello',
+    ...attrs
+  }
+
+  const handlers = {
+    onSelect: action('onSelect'),
+  }
+
+  return <TodoFilterButton {...props} {...handlers} />
+};
+
+storiesOf('TodoFilterButton', module)
+  .add('default', () => buildStory())
+  .add('selected', () => buildStory({ label: 'Selected', isSelected: true }));
