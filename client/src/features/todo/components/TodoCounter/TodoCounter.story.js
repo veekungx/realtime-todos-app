35eff1811638f41950af50e97040dfa080016ace
Ti <veekungx@gmail.com>
2017-10-07 20:08:40 +0700

    Add TodoCounter story

---
+++ client/src/features/todo/components/TodoCounter/TodoCounter.story.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import TodoCounter from './TodoCounter';
+
+storiesOf('TodoCounter', module)
+  .add('zero todos', () => <TodoCounter counter={0} />)
+  .add('a todo', () => <TodoCounter counter={1} />)
+  .add('2 or more todos', () => <TodoCounter counter={2} />);
