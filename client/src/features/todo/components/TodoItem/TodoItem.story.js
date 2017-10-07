ae52aa664f07041ff7da5d6f7205f47f07e90e80
Ti <veekungx@gmail.com>
2017-10-07 14:52:58 +0700

    TodoItem story

--- client/src/features/todo/components/TodoItem/TodoItem.story.js
+++ client/src/features/todo/components/TodoItem/TodoItem.story.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react'; 
+import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import TodoItem from "./TodoItem";
+import TodoItem from './TodoItem';
 
-const props = {};
-const events = {};
+const buildStory = (attrs) => {
+  const todo = {
+    id: Math.round(Math.random() * 1000000).toString(),
+    title: 'Hello World',
+    updatedAt: Date.now(),
+    ...attrs,
+  }
 
-storiesOf("TodoItem",module)
-  .add("Default",()=> <TodoItem {...props} {...events}/>);
+  const props = {
+    todo
+  }
+  
+  const handlers = {
+    onDeleteTodo: action('onDeleteTodo'),
+    onToggleTodo: action('onToggleTodo'),
+  }
+
+  return <TodoItem {...props} {...handlers} />
+};
+
+storiesOf('TodoItem', module)
+  .add('TODO_ACTIVE', () => buildStory({ state: 'TODO_ACTIVE' }))
+  .add('TODO_COMPLETED', () => buildStory({ state: 'TODO_COMPLETED' }));
