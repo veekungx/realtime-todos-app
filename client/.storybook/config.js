1d8dcbdac57d0f5567c4cf0f3feb6cd3925d12ca
Ti <veekungx@gmail.com>
2017-10-08 04:55:04 +0700

    Improve Storybook to support apollo

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,4 +1,10 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import apolloStorybookDecorator from 'apollo-storybook-decorator';
+import typeDefs from '../src/typeDefs';
+
+addDecorator(
+  apolloStorybookDecorator({ typeDefs, mocks: {} })
+);
 
 const req = require.context("../src", true, /\.story\.js$/);
 const loadStories = () => {
--- client/src/helpers/generateMockTodo.test.js
+++ client/src/helpers/generateMockTodo.test.js
@@ -1,7 +1,6 @@
-import generateMockTodo from './generateMockTodo';
+import { generateMockTodo, generateMockNodeTodo } from './generateMockTodo';
 
 describe('generateMockTodo', () => {
-
   it('should be function', () => {
     expect(generateMockTodo).toEqual(expect.any(Function));
   });
@@ -21,5 +20,28 @@ describe('generateMockTodo', () => {
     expect(todo.id).toEqual(attrs.id);
     expect(todo.title).toEqual(attrs.title);
     expect(todo.state).toEqual(attrs.state);
+  });
+});
+
+describe('generateMockNodeTodo', () => {
+  it('should be function', () => {
+    expect(generateMockNodeTodo).toEqual(expect.any(Function));
+  });
+
+  it('should create mock todo', () => {
+    const nodeTodo = generateMockNodeTodo();
+    expect(nodeTodo).toBeDefined();
+  });
+
+  it('should overwrite todo properties when given attributes', () => {
+    const attrs = {
+      id: 100,
+      title: 'Custom Title',
+      state: 'TODO_COMPLETD'
+    }
+    const todo = generateMockNodeTodo(attrs);
+    expect(todo.node.id).toEqual(attrs.id);
+    expect(todo.node.title).toEqual(attrs.title);
+    expect(todo.node.state).toEqual(attrs.state);
   });
 });
