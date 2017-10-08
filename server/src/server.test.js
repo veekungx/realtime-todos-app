a3045833f77eb1dbcbb30942029f0975738ea3c8
Ti <veekungx@gmail.com>
2017-10-08 19:20:43 +0700

    Testing Query.todos

--- server/src/server.test.js
+++ server/src/server.test.js
@@ -1,6 +1,8 @@
 const { expect } = require('chai');
 const request = require('supertest');
 const server = require('./server');
+const mongoose = require('mongoose');
+const { TodoModel } = require('./models');
 
 describe('GET /status', () => {
   it('should response OK', async () => {
@@ -13,30 +15,45 @@ describe('GET /status', () => {
 
 describe('GraphQL', () => {
   describe('Query', () => {
-    it('should return todos', async () => {
-      const response = await request(server)
-        .post('/graphql')
-        .send({
-          query: `
-            {
-              todos{
-                edges{
-                  node{
-                    id
-                    title
+    describe('todos', () => {
+      beforeEach(async () => {
+        await TodoModel.remove({});
+      });
+      it('should return todos', async () => {
+        const todo = new TodoModel({ title: 'Hi', state: 'TODO_ACTIVE' });
+        await todo.save();
+        const response = await request(server)
+          .post('/graphql')
+          .send({
+            query: `
+              {
+                todos{
+                  edges{
+                    node{
+                      id
+                      title
+                      state
+                    }
                   }
                 }
               }
-            }
-          `
-        })
-      const actualResult = response.body.data;
-      const expectedResult = {
-        todos: {
-          edges: []
-        }
-      };
-      expect(actualResult).to.eql(expectedResult);
-    });
+            `
+          });
+
+        const actualResult = response.body.data;
+        const expectedResult = {
+          todos: {
+            edges: [{
+              node: {
+                id: todo._id.toString(),
+                title: 'Hi',
+                state: 'TODO_ACTIVE'
+              }
+            }]
+          }
+        };
+        expect(actualResult).to.eql(expectedResult);
+      });
+    })
   });
 })
