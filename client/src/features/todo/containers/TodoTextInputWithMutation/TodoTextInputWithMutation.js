ec6aeeabfe5f6ea2f4f8c328a39b45c3704a8923
Ti <veekungx@gmail.com>
2017-10-09 01:32:15 +0700

    Update cache when add Todo

--- client/src/features/todo/containers/TodoTextInputWithMutation/TodoTextInputWithMutation.js
+++ client/src/features/todo/containers/TodoTextInputWithMutation/TodoTextInputWithMutation.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { withState, compose } from 'recompose';
 import TodoTextInput from '../../components/TodoTextInput/TodoTextInput';
 import { gql, graphql } from 'react-apollo';
-
+import { TODO_LIST_QUERY } from '../TodoListWithData/TodoListWithData';
 const mutation = gql`
   mutation TodoTextInputWithMutation($title :String!){
     createTodo(title: $title){
@@ -13,10 +13,29 @@ const mutation = gql`
   }
 `;
 
+const mutationOptions = {
+  name: 'onSubmit',
+  options: {
+    update: (proxy, { data: { createTodo } }) => {
+      const data = proxy.readQuery({ query: TODO_LIST_QUERY })
+      console.log(createTodo);
+      data.todos.edges.push({
+        node: createTodo,
+        __typename: "TodoEdge"
+      });
+      proxy.writeQuery({ query: TODO_LIST_QUERY, data });
+    },
+    // optimisticResponse: {
+    //   createTodo: {
+    //     id: -1,
+    //     title: "Hate",
+    //   }
+    // }
+  }
+}
+
 
 export default compose(
   withState('value', 'onChangeText', ''),
-  graphql(mutation, {
-    name: 'onSubmit'
-  })
+  graphql(mutation, mutationOptions)
 )(TodoTextInput);
