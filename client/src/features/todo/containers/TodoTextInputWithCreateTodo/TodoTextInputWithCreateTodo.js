4b55546d2c0e477a6507f4287b8f6b40223bb15a
Tharin Taechanadit <tharin.tae@carmana.com>
2017-10-12 12:56:33 +0700

    refactor TodoTextInput to use connect

--- client/src/features/todo/containers/TodoTextInputWithCreateTodo/TodoTextInputWithCreateTodo.js
+++ client/src/features/todo/containers/TodoTextInputWithCreateTodo/TodoTextInputWithCreateTodo.js
@@ -1,12 +1,26 @@
 import { withHandlers, withState, compose } from 'recompose';
 import { graphql } from 'react-apollo';
+import { connect } from 'react-redux';
+
+import { setText } from '../../reducer';
 import TodoTextInput from '../../components/TodoTextInput/TodoTextInput';
 import CreateTodoMutation from './CreateTodo.mutation.gql';
-import TodoWithDataQuery from '../TodoWithData/TodoWithData.query.gql';
+
+const mapState = (state) => {
+  return {
+    value: state.todo.text
+  }
+}
+
+const mapDispatch = (dispatch) => {
+  return {
+    onChangeText: (text) => dispatch(setText(text))
+  }
+}
 
 export default compose(
   graphql(CreateTodoMutation, { name: 'createTodo' }),
-  withState('value', 'onChangeText', ''),
+  connect(mapState, mapDispatch),
   withHandlers({
     onSubmit: props => event => {
       if (!props.value) return;
