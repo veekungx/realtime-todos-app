d7f5095d7f07b7a7f535919eda6d43620bfabf6f
Tharin Taechanadit <tharin.tae@carmana.com>
2017-10-12 11:36:45 +0700

    refactor apollo config

--- client/src/index.js
+++ client/src/index.js
@@ -1,31 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo';
-import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws';
+import { ApolloProvider } from 'react-apollo';
+import { store, client } from './config-apollo';
 
 import './index.scss';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-const networkInterface = createNetworkInterface({
-  uri: 'http://localhost:4000/graphql',
-})
-
-const wsClient = new SubscriptionClient('ws://localhost:4000/subscriptions', {
-  reconnect: true,
-})
-
-const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
-  networkInterface,
-  wsClient
-)
-
-const client = new ApolloClient({
-  networkInterface: networkInterfaceWithSubscriptions
-})
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
+  <ApolloProvider store={store} client={client} >
     <App />
   </ApolloProvider>
   , document.getElementById('root'));
