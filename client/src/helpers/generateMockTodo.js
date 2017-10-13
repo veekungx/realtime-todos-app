80c90326ae44ab395c61325c59f4fb866db4150e
Ti <veekungx@gmail.com>
2017-10-14 03:19:26 +0700

    Fix graphql components test

--- client/src/helpers/generateMockTodo.js
+++ client/src/helpers/generateMockTodo.js
@@ -1,7 +1,7 @@
 export const generateMockTodo = (attrs) => {
   const todo = {
-    id: Math.round(Math.random() * 100000),
-    title: 'Title ' + Math.round(Math.random() * 100000),
+    id: Math.round(Math.random() * 100000).toString(),
+    title: 'Title ' + Math.round(Math.random() * 100000).toString(),
     state: 'TODO_ACTIVE',
   }
   return {
