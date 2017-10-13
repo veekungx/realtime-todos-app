ca40abcfdff447f4c6cf8d367faee25a50814ab1
Ti <veekungx@gmail.com>
2017-10-14 03:54:53 +0700

    test App component

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 import './App.scss';
-import { FortuneTellerWithQuery } from '../FortuneTeller/FortuneTeller';
+import FortuneTellerWithQuery from '../../containers/FortuneTellerWithQuery/FortuneTellerWithQuery';
 import TodoWithData from '../../features/todo/containers/TodoWithData/TodoWithData';
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <TodoWithData />
-        <FortuneTellerWithQuery />
-      </div>
-    );
-  }
-}
+
+const App = () =>
+  <div className="App">
+    <TodoWithData />
+    <FortuneTellerWithQuery />
+  </div>
 
 export default App;
---
+++ client/src/components/App/App.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+import TodoWithData from '../../features/todo/containers/TodoWithData/TodoWithData'
+import FortuneTellerWithQuery from '../../containers/FortuneTellerWithQuery/FortuneTellerWithQuery'
+
+describe('App', () => {
+  describe('Components', () => {
+    it('should have TodoWithData', () => {
+      const wrapper = shallow(<App />);
+      expect(wrapper.find(TodoWithData).exists()).toEqual(true);
+    });
+    it('should have FortuneTellerWithQuery', () => {
+      const wrapper = shallow(<App />);
+      expect(wrapper.find(FortuneTellerWithQuery).exists()).toEqual(true);
+    });
+  });
+});
